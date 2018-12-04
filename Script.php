d34f3bbc3c3a771e859a6b79dec7503acc47cd50
Isrg Rajan <isrgrajan@outlook.com>
2018-12-04 12:31:07 +0530

    Create Script.php

---
+++ Script.php
@@ -0,0 +1,34 @@
+<?php
+$google_news_API="YOUR API HERE";
+$search_keyword="Apple in India"; 
+$news_count=1; //Set the News result count
+$days_ago = date('Y-m-d', strtotime('-2 days')); //Set date for past news
+
+$search_keyword=str_replace(" ","+",$search_keyword);
+$json_response=file_get_contents("https://newsapi.org/v2/everything?q=".$search_keyword."&from=".$days_ago."&sortBy=date&apiKey=".$google_news_API);
+$json_array=json_decode($json_response,true);
+$nos_of_post=0;
+$news_count=$news_count*5;
+$news=array();
+foreach($json_array as $ja)
+{
+  if(is_array($ja)){
+  foreach($ja as $response)
+  {     
+                  if(is_array($response)){
+                          foreach($response as $res)
+                          {
+                              if(!is_array(($res)))
+                              {
+                              if($nos_of_post==$news_count) break;
+                               array_push($news,$res);
+                              $nos_of_post++;
+                              }                           
+                              
+                          }
+                 }     
+         }
+     } 
+}
+var_dump($news);
+?>
